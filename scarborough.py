b93d44ad2cbf585778405fff17f1c82223d6f525
lucas <lucasjoao.lj@gmail.com>
2017-08-28 09:41:03 -0300

    add scarborough

---
+++ scarborough.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+
+def scarborough(matrix):
+    """Critério de Scarborough. Se há diagonal dominante retorna 1, senão 0."""
+
+    i_j = matrix.shape
+
+    doms = est_doms = []
+
+    for i in range(i_j[0]):
+        sum_line = 0
+        for j in range(i_j[1]):
+            sum_line += matrix.item(i, j) if i != j else 0
+
+        dom = matrix.item(i, i) >= sum_line
+        est_dom = matrix.item(i, i) > sum_line
+
+        doms.append(dom)
+        est_doms.append(est_dom)
+
+        if dom is False:
+            break
+
+    if False in doms or est_doms == [i_j[0] * False]:
+        return 0
+
+    return 1
