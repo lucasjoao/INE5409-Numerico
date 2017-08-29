69c1dff7585df75e4ddd18a3df9acf5c8897f4ea
AdrianoTosetto <adriano.rafael10@hotmail.com>
2017-08-29 01:06:00 -0300

    geração de coeficientes

--- equations.py
+++ equations.py
@@ -1,14 +1,29 @@
 import numpy as np
 
 def equations(n):
-	matrix = np.zeros((n, n+1))
+    matrix = np.zeros((n,n+1))
     matrix[0,0] = 1
     matrix[0,1] = 1
+    matrix[0, n] = 1.5
 
     for i in range(1, int(n/2)):
-        x[i, n + 1] = 1;
-        x[i, i - 1] = 1;
-        x[i, i + 1] = 1;
-        x[i, i + 25] = 1;
-        x[i, i] = 4;
-	print(matrix)
+        matrix[i,i] = 4
+        matrix[i, i+25] = 1
+        matrix[i, i+1] = 1
+        matrix[i, i-1] = 1
+        matrix[i,n] = 1
+    
+    for i in range(int(n/2), n-1):
+        matrix[i,i] = 5
+        matrix[i, i-25] = 1
+        matrix[i, i+1] = 1
+        matrix[i, i-1] = 1
+        matrix[i, n] = 2
+        #b[i] = 2
+
+    matrix[n-1, n-1] = 1
+    matrix[n-1, n-2] = 1
+    matrix[n-1,n] = 3
+
+    print(matrix)
+    return matrix
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import gauss_elimination as ge
 import equations as eqs
 import numpy as np
+import test as tst
 
 if __name__ == "__main__":
-	print(eqs.equations(50))
+	eqs.equations(50)
+	print("fine:")
+	print(tst.matriz_coeficientes(50))
